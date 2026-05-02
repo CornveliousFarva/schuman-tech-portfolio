9f2e29e6b9d3d90c8f35e06f92433da759022028
Greg Schuman <greg.c.schuman@gmail.com>
2026-05-02 13:31:02 -0700

    Fixed routings in the Home file of the Pages folder.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
-import Navbar from "@/components/Navbar";
+import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import Services from "@/components/Services";
-import Portfolio from "@/components/Portfolio";
-import About from "@/components/About";
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer"; 
+import Services from "../components/Services";
+import Portfolio from "../components/Portfolio";
+import About from "../components/About";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer"; 
 
 export default function Home() {
   return (
