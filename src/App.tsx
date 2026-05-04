716a42f9d026070290a6932da67abad8855022b7
Greg Schuman <greg.c.schuman@gmail.com>
2026-05-03 19:47:20 -0700

    Incorporated react router dom into app.tsx, which will now render all of the pages and components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
-import PortfolioPage from "./pages/PortfolioPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Portfolio from "./pages/PortfolioPage";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-export default function App() {
-  return <PortfolioPage />;
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen bg-white text-slate-900 dark:bg-slate-950 dark:text-white">
+        <Navbar />
+
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
 }
+
+export default App;
