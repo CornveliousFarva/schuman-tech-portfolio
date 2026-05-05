c90dec9d9c1031dde0bb95f2023dfb6556cdf11b
Greg Schuman <greg.c.schuman@gmail.com>
2026-05-04 19:49:33 -0700

    Moved hero component to the home page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,21 @@
-// import { Link } from "react-router-dom";
+import Hero from "../components/Hero";
 
 export default function Home() {
   return (
-    <div>
+    <>
+      <Hero />
+
       <section id="services" className="mx-auto max-w-6xl px-6 py-16">
-        <h2 className="text-3xl font-bold text-slate-900 dark:text-white">
-          Services
-        </h2>
+        <h2 className="text-3xl font-bold text-white">Services</h2>
       </section>
 
       <section id="about" className="mx-auto max-w-6xl px-6 py-16">
-        <h2 className="text-3xl font-bold text-slate-900 dark:text-white">
-          About
-        </h2>
+        <h2 className="text-3xl font-bold text-white">About</h2>
       </section>
 
       <section id="contact" className="mx-auto max-w-6xl px-6 py-16">
-        <h2 className="text-3xl font-bold text-slate-900 dark:text-white">
-          Contact
-        </h2>
+        <h2 className="text-3xl font-bold text-white">Contact</h2>
       </section>
-    </div>
+    </>
   );
 }
