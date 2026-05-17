0a276ddade8e9ad1c11f3f0d163d6fc0eb503aa3
Greg Schuman <greg.c.schuman@gmail.com>
2026-05-17 14:00:00 -0700

    Fixed the routing issues with the images. Or so I hope.

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,13 +7,13 @@ const projects = [
     technologies: ['Python', 'Pygame', 'OOP'],
 
     images: [
-      '/images/pong/start-screen.png',
+      '/images/pong/pongstart-screen.png',
       '/images/pong/pong-classic.png',
-      '/images/pong/australian-open.png',
-      '/images/pong/french-open.png',
-      '/images/pong/wimbledon.png',
-      '/images/pong/us-open.png',
-      '/images/pong/win-screen.png',
+      '/images/pong/pong-australian-open.png',
+      '/images/pong/pong-french-open.png',
+      '/images/pong/pong-wimbledon.png',
+      '/images/pong/pong-us-open.png',
+      '/images/pong/pong-win-screen.png',
     ],
 
     github: 'https://github.com/CornveliousFarva/pong',
